9421eeea6cb9451103fdf7bf1e748cfe748c5415
Matt <m.wilshirejones@gmail.com>
2018-03-25 11:51:19 +0200

    Ficxed cell tests

--- app/test/cells/project_front_end_cell_test.rb
+++ app/test/cells/project_front_end_cell_test.rb
@@ -1,8 +1,12 @@
 require 'test_helper'
 
 class ProjectFrontEndCellTest < Cell::TestCase
+  setup do
+    @project = projects(:stills_project)
+  end
+
   test 'it shows' do
-    html = cell(:project_front_end).call(:show)
-    # assert html.contains(/<html>/)
+    html = cell(:project_front_end, @project).call(:show)
+    assert html.has_xpath?('//html')
   end
 end
