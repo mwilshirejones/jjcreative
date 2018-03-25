646fd3c64aa9834b807aba790869111bd1c5e7ba
Matt <m.wilshirejones@gmail.com>
2018-03-25 18:20:41 +0200

    Added image model/tests and carrierwave

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :images, except: [:show]
   resources :stills_projects, except: [:index, :show]
   resources :motion_projects, except: [:index, :show]
   
--- app/db/schema.rb
+++ app/db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180324211058) do
+ActiveRecord::Schema.define(version: 20180325151509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "images", force: :cascade do |t|
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "portfolios", force: :cascade do |t|
     t.string "title"
