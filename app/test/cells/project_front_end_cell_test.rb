bcffa3232d81d0058897785b18d51e4fc699b67d
Matt <m.wilshirejones@gmail.com>
2018-09-24 13:42:43 +0200

    Added test for markdown rendering method

--- app/test/cells/project_front_end_cell_test.rb
+++ app/test/cells/project_front_end_cell_test.rb
@@ -9,4 +9,15 @@ class ProjectFrontEndCellTest < Cell::TestCase
     html = cell(:project_front_end, @project).call(:show)
     assert html.has_xpath?('//html')
   end
+
+  test 'it renders description markdown as HTML' do
+    @project.description = 'This is *bongos*, indeed.'
+    html = cell(:project_front_end, @project).call(:show)
+
+    has_em_element = html.all(:xpath, '//em').map do |element|
+      element.text == 'bongos'
+    end.compact.first
+
+    assert has_em_element
+  end
 end
