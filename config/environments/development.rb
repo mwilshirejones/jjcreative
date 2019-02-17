60561fbf327e6875a249b918485ef6e8ee4c68e8
Matt <m.wilshirejones@gmail.com>
2019-02-17 11:24:48 +0200

    Serve static assets for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Serve static assets from Rails, in production use nginx instead
+  config.serve_static_assets = true
 end
